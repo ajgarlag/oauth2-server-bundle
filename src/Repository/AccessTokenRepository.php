1d6f58757cae1de41447c24dcb0301b3f70c976c
Antonio J. García Lagar <aj@garcialagar.es>
2026-07-02 09:26:51 +0200

    Add `EventDispatcherInterface` argument to `AccessTokenRepository::__construct()`

--- src/Repository/AccessTokenRepository.php
+++ src/Repository/AccessTokenRepository.php
@@ -36,7 +36,7 @@ final class AccessTokenRepository implements AccessTokenRepositoryInterface
     private $scopeConverter;
 
     /**
-     * @var EventDispatcherInterface|null
+     * @var EventDispatcherInterface
      */
     private $eventDispatcher;
 
@@ -44,33 +44,20 @@ final class AccessTokenRepository implements AccessTokenRepositoryInterface
         AccessTokenManagerInterface $accessTokenManager,
         ClientManagerInterface $clientManager,
         ScopeConverterInterface $scopeConverter,
-        ?EventDispatcherInterface $eventDispatcher = null,
+        EventDispatcherInterface $eventDispatcher,
     ) {
         $this->accessTokenManager = $accessTokenManager;
         $this->clientManager = $clientManager;
         $this->scopeConverter = $scopeConverter;
-        if (null === $eventDispatcher) {
-            trigger_deprecation(
-                'league/oauth2-server-bundle',
-                '1.2',
-                'The "%s" class constructor will require a new "%s $eventDispatcher" argument in 2.0. Not passing it is deprecated',
-                self::class,
-                EventDispatcherInterface::class
-            );
-        }
         $this->eventDispatcher = $eventDispatcher;
     }
 
     public function getNewToken(ClientEntityInterface $clientEntity, array $scopes, ?string $userIdentifier = null): AccessTokenEntityInterface
     {
-        if (null !== $this->eventDispatcher) {
-            $event = $this->eventDispatcher->dispatch(
-                new AccessTokenExtraClaimsResolveEvent($clientEntity, $scopes, $userIdentifier),
-                OAuth2Events::ACCESS_TOKEN_EXTRA_CLAIMS_RESOLVE
-            );
-        } else {
-            $event = null;
-        }
+        $event = $this->eventDispatcher->dispatch(
+            new AccessTokenExtraClaimsResolveEvent($clientEntity, $scopes, $userIdentifier),
+            OAuth2Events::ACCESS_TOKEN_EXTRA_CLAIMS_RESOLVE
+        );
 
         $accessToken = new AccessTokenEntity($event?->getExtraClaims() ?? []);
         $accessToken->setClient($clientEntity);
